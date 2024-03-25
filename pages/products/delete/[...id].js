48030aebb206a2136343b17334109dc95a2bc304
Nguyen Hoang Long <nguyenlong0920@gmail.com>
2024-03-25 17:36:39 +0700

    fix &quot;

--- pages/products/delete/[...id].js
+++ pages/products/delete/[...id].js
@@ -26,7 +26,7 @@ export default function DeleteProductPage() {
     }
     return (
         <Layout>
-            <h1 className="text-center">Do you want to delete product &nbsp;"{productInfo?.title}"?</h1>
+            <h1 className="text-center">Do you want to delete product &nbsp;&quot;{productInfo?.title}&quot;?</h1>
             <div className="flex gap-2 justify-center">
                 <button className="btn-red" onClick={deleteProduct}>Yes</button>
                 <button className="btn-default" onClick={goBack}>No</button>
