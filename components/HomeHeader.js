7e72dd94861258b0175ba32e66d72b8dea46e7cc
Nguyen Hoang Long <nguyenlong0920@gmail.com>
2024-03-26 21:03:38 +0700

    test

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,54 +1,18 @@
-// import {useSession} from "next-auth/react";
-
-// export default function HomeHeader() {
-//     const {data:session} = useSession();
-//     return (
-//         <div className="text-blue-900 flex-col justify-between">
-//             <div className="hidden sm:block mb-8">
-//                 <div className="flex justify-center overflow-hidden">
-//                     <img src={session?.user?.image} alt="" className="w-24 h-24 rounded-3xl"/>
-//                 </div>
-//             </div>
-//             <h2 className="mt-0">
-//                 <div className="flex gap-4 items-center justify-center">
-//                     <img src={session?.user?.image} alt="" className="w-10 h-10 rounded-md sm:hidden"/>
-//                     <div className='text-2xl'>
-//                         Hello, <b>{session?.user?.name}</b>
-//                     </div>
-//                 </div>
-//             </h2>
-//         </div>
-//     );
-// }
-
-import { useSession } from "next-auth/react";
-import Image from "next/image"; // Import the Image component
+import {useSession} from "next-auth/react";
 
 export default function HomeHeader() {
-    const { data: session } = useSession();
+    const {data:session} = useSession();
     return (
         <div className="text-blue-900 flex-col justify-between">
             <div className="hidden sm:block mb-8">
                 <div className="flex justify-center overflow-hidden">
-                    <Image
-                        src={session?.user?.image}
-                        alt=""
-                        width={24} // Set your desired width
-                        height={24} // Set your desired height
-                        className="rounded-3xl"
-                    />
+                    <img src={session?.user?.image} alt="" className="w-24 h-24 rounded-3xl"/>
                 </div>
             </div>
             <h2 className="mt-0">
                 <div className="flex gap-4 items-center justify-center">
-                    <Image
-                        src={session?.user?.image}
-                        alt=""
-                        width={10} // Set your desired width
-                        height={10} // Set your desired height
-                        className="rounded-md sm:hidden"
-                    />
-                    <div className="text-2xl">
+                    <img src={session?.user?.image} alt="" className="w-10 h-10 rounded-md sm:hidden"/>
+                    <div className='text-2xl'>
                         Hello, <b>{session?.user?.name}</b>
                     </div>
                 </div>
@@ -56,3 +20,39 @@ export default function HomeHeader() {
         </div>
     );
 }
+
+// import { useSession } from "next-auth/react";
+// import Image from "next/image"; // Import the Image component
+
+// export default function HomeHeader() {
+//     const { data: session } = useSession();
+//     return (
+//         <div className="text-blue-900 flex-col justify-between">
+//             <div className="hidden sm:block mb-8">
+//                 <div className="flex justify-center overflow-hidden">
+//                     <Image
+//                         src={session?.user?.image}
+//                         alt=""
+//                         width={24} // Set your desired width
+//                         height={24} // Set your desired height
+//                         className="rounded-3xl"
+//                     />
+//                 </div>
+//             </div>
+//             <h2 className="mt-0">
+//                 <div className="flex gap-4 items-center justify-center">
+//                     <Image
+//                         src={session?.user?.image}
+//                         alt=""
+//                         width={10} // Set your desired width
+//                         height={10} // Set your desired height
+//                         className="rounded-md sm:hidden"
+//                     />
+//                     <div className="text-2xl">
+//                         Hello, <b>{session?.user?.name}</b>
+//                     </div>
+//                 </div>
+//             </h2>
+//         </div>
+//     );
+// }
